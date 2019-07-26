3f01cb0527d9fca0ccbd17d5d9f642d12620b5eb
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-26 08:36:35 -0700

    Added CharacterCards for each character in api

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+const CharName = styled.h2`
+  font-size: 2.4rem;
+`;
+
+const CharInfo = styled.p`
+  font-size: 1.4rem;
+`;
+
+export default function CharacterCard ({ name, species, origin }) {
+
+  return (
+  <div>
+    <CharName>{name}</CharName>
+    <CharInfo>{species}</CharInfo>
+    <CharInfo>{origin}</CharInfo>
+  </div>
+  )
 }
